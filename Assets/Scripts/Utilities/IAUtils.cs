7d3697a75e885fde942ce05fdae98ed788270e8f
Bruno Cesar Pimenta Fernandes <brunocpf@dcc.ufmg.br>
2018-04-24 23:34:44 -0300

    - add docs directory - import basic assets

---
+++ Assets/Scripts/Utilities/IAUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class IAUtils
+{
+	public static float NormalizeValue(float value, float minValue, float maxValue) {
+		return Mathf.InverseLerp(minValue, maxValue, value);
+	}
+
+	public static float NormalizeValue(float value, float minValue, float maxValue, float a, float b) {
+		return (b - a) * ((value - minValue)/(maxValue - minValue)) + a;
+	}
+}
